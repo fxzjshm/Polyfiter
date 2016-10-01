997b5f16e40f248cd6de86b40e94c8d72d81f688
fxzjshm <fxzjshm@163.com>
2016-10-01 17:34:50 +0800

    Add point (0,0) and swiched to open-source MOE backend.

--- core/src/com/entermoor/polyfiter/Polyfiter.java
+++ core/src/com/entermoor/polyfiter/Polyfiter.java
@@ -3,6 +3,7 @@ package com.entermoor.polyfiter;
 import com.badlogic.gdx.Application;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.Color;
@@ -12,7 +13,6 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Touchpad;
@@ -24,12 +24,11 @@ import com.kotcrab.vis.ui.VisUI;
 import java.util.List;
 
 public class Polyfiter extends ApplicationAdapter {
-    public static List</*Point*/ ?> points;
-
+    public static List<Point> points;
 
     public SpriteBatch batch;
     public InputProcessor inputProcessor;
-    //	public Texture img;
+//    	public Texture img;
 
     public Stage stage;
     public Viewport viewport;
@@ -42,21 +41,36 @@ public class Polyfiter extends ApplicationAdapter {
     public ShapeRenderer shapeRenderer;
 
     public Color pointColor, xColor, yColor;
+    public float scaleDeltaXY = 1;
+
+    public static boolean isPointInScreen(float x, float y, float centerX, float centerY, float width, float height) {
+        /* Step 1:                     Step 2:                         Step 3:                       Step 4:
+        *   |                              |                          ---------                     ---------
+        *   | @                          @ |                              @                             @
+        *   |                              |
+        * */
+        if (centerX - (width / 2) < x && x < centerX + (width / 2) && centerY - (height / 2) < y && y < centerY + (height / 2))
+            return true;
+        return false;
+    }
 
     @Override
     public void create() {
         Gdx.app.setLogLevel(Application.LOG_DEBUG);
-//        Gdx.app.debug("working directory", Gdx.files.internal("").file().getAbsolutePath());
+//        Gdx.app.debug("Working Directory", Gdx.files.internal("").file().getAbsolutePath());
+
+        Gdx.graphics.setWindowedMode(Gdx.graphics.getDisplayMode().width, Gdx.graphics.getDisplayMode().height);
 
         batch = new SpriteBatch();
 //		img = new Texture("badlogic.jpg");
         viewport = new ScreenViewport();
         stage = new Stage(viewport, batch);
-
+        stage.setDebugAll(true);
 
         innerCamera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight() / 5 * 3);
         innerViewport = new ScreenViewport(innerCamera);
         innerStage = new Stage(innerViewport, batch);
+        innerStage.setDebugAll(true);
         shapeRenderer = new ShapeRenderer();
 
         VisUI.load();
@@ -64,7 +78,7 @@ public class Polyfiter extends ApplicationAdapter {
         img = new Image(new TextureRegionDrawable(new TextureRegion(new Texture("badlogic.jpg"))));
         innerStage.addActor(img);
 
-        touchpad = new Touchpad(Gdx.graphics.getWidth() / 5, VisUI.getSkin());
+        touchpad = new Touchpad(Math.min(Gdx.graphics.getWidth(), Gdx.graphics.getHeight()) / 5, VisUI.getSkin());
 //        touchpad.setBounds( /* touchpad.getDeadzone() */ Gdx.graphics.getWidth() / 5, Gdx.graphics.getWidth() / 5, touchpad.getWidth(), touchpad.getHeight());
         touchpad.getColor().a *= 0.233;
         stage.addActor(touchpad);
@@ -81,7 +95,8 @@ public class Polyfiter extends ApplicationAdapter {
         viewport.update(width, height);
         touchpad.setDeadzone(width / 5);
 //        touchpad.setBounds(width / 5, width / 5, width / 5 * 2, width / 5 * 2);
-        touchpad.setBounds(0, 0, width / 5 * 2, width / 5 * 2);
+        touchpad.setBounds(Math.min(width, height) / 5, Math.min(width, height) / 5, Math.min(width, height) / 5 * 2, Math.min(width, height) / 5 * 2);
+        Gdx.app.debug("Resize", "Width: " + width + ", Height: " + height + ".");
     }
 
     @Override
@@ -90,8 +105,25 @@ public class Polyfiter extends ApplicationAdapter {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         if (touchpad.isTouched()) {
-//            Gdx.app.debug("Touchpad", touchpad.getKnobPercentX() + ", " + touchpad.getKnobPercentY());
-            innerCamera.translate(touchpad.getKnobPercentX(), touchpad.getKnobPercentY());
+            Gdx.app.debug("Touchpad", touchpad.getKnobPercentX() + ", " + touchpad.getKnobPercentY());
+            float deltaX = touchpad.getKnobPercentX();
+            float deltaY = touchpad.getKnobPercentY();
+            scaleDeltaXY += Math.abs((deltaX + deltaY)) / 20;
+            innerCamera.translate(deltaX * scaleDeltaXY, deltaY * scaleDeltaXY);
+        } else if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            scaleDeltaXY += 0.1;
+            innerCamera.translate(-1 * scaleDeltaXY, 0);
+        } else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            scaleDeltaXY += 0.1;
+            innerCamera.translate(1 * scaleDeltaXY, 0);
+        } else if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            scaleDeltaXY += 0.1;
+            innerCamera.translate(0, 1 * scaleDeltaXY);
+        } else if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            scaleDeltaXY += 0.1;
+            innerCamera.translate(0, -1 * scaleDeltaXY);
+        } else {
+            scaleDeltaXY = 1;
         }
 
         batch.begin();
@@ -103,10 +135,15 @@ public class Polyfiter extends ApplicationAdapter {
         shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
         shapeRenderer.setColor(1, 1, 0, 1);
 // TODO x and y line.
+        if (isPointInScreen(0, 0, innerCamera.position.x, innerCamera.position.y, Gdx.graphics.getWidth(), Gdx.graphics.getHeight())){
+
+        }
         shapeRenderer.end();
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
         shapeRenderer.setColor(0, 1, 0, 1);
 // TODO points?
+        shapeRenderer.circle(0, 0, 2.33F);
+//        shapeRenderer.circle(innerCamera.position.x, innerCamera.position.y, 6.666F);
         shapeRenderer.end();
 
 //        innerStage.draw();
@@ -121,5 +158,14 @@ public class Polyfiter extends ApplicationAdapter {
         batch.dispose();
 //        img.dispose();
         VisUI.dispose();
+    }
+
+    public static class Point {
+        public double x, y = 0;
+
+        public Point(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
     }
 }
